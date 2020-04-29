9e5d272e34518b9279d87372c1df25c96f9ca20b
巫政霖 <wuzhenglin510@163.com>
2020-04-29 17:31:57 +0800

    fix

--- src/main/java/com/wzl/jacoco/controller/BookRepositoryController.java
+++ src/main/java/com/wzl/jacoco/controller/BookRepositoryController.java
@@ -3,6 +3,7 @@ package com.wzl.jacoco.controller;
 import com.wzl.jacoco.entity.BookBasicInfo;
 import com.wzl.jacoco.entity.BookStorageInfo;
 import com.wzl.jacoco.storage.BookRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,11 +14,13 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "repository")
+@Slf4j
 public class BookRepositoryController {
 
     @GetMapping("get_book")
     public BookBasicInfo getBook(@RequestParam(value = "bookName") String bookName) {
         BookStorageInfo bookStorageInfo = BookRepository.getBookStorageInfo(bookName);
+        log.info("我觉得我们需要加点什么功能在这里");
         //我是注释啦啦啦
         /*
         我还是注释啦
@@ -27,6 +30,7 @@ public class BookRepositoryController {
 
     @GetMapping("list_books")
     public List<BookBasicInfo> listBooks() {
+        log.info("我觉得我们需要加点什么功能在这里");
         List<BookStorageInfo> bookStorageInfos = BookRepository.listBookStorageInfo();
         return bookStorageInfos.stream()
                 .map(item -> new BookBasicInfo(item.getBookName(), item.getStock()))
